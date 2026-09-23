b02a1416d5e277c6555937109c7efb7da6881797
Miguel Angel Escobar Lazcano <m.escobar_27@outlook.com>
2026-09-23 15:42:50 -0400

    aclara en el aviso de orden de trabajo que las hectáreas son las de toda la tanda

--- lang/es/notificaciones.php
+++ lang/es/notificaciones.php
@@ -3,7 +3,7 @@
 return [
     'titulo' => [
         'contrato_creado' => 'Contrato nuevo de :cliente (:hectareas ha).',
-        'orden_trabajo_creada' => 'Tu cuadrilla tiene una orden de trabajo nueva: aplicación :aplicacion de la orden #:orden (:hectareas ha).',
+        'orden_trabajo_creada' => 'Tu cuadrilla tiene una orden de trabajo nueva: aplicación :aplicacion de la orden #:orden (:hectareas ha en total).',
         'trabajo_cerrado' => 'Se cerró un trabajo de :hectareas ha (aplicación :aplicacion, orden #:orden).',
     ],
 ];
